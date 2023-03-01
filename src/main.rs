0f5e5ffe0c2e1d2cd9bfc55c4f4b2faaf5078624
rodrigocitadin <citadinrodrigo@gmail.com>
2023-03-01 16:49:56 -0300

    finally finished

--- src/main.rs
+++ src/main.rs
@@ -2,51 +2,25 @@ use std::io;
 
 fn main() {
     loop {
-        println!("\nWhat you want to know?\n");
-        println!("\nIs it prime number? (1)       N prime number (2)\n");
+        println!("\nEnter a number and I'll tell you if it's odd\n");
 
-        let mut choice: String = String::new();
+        let mut num: String = String::new();
 
         io::stdin()
-            .read_line(&mut choice)
-            .expect("Falha ao ler entrada");
+            .read_line(&mut num)
+            .expect("Unexpected Error...");
 
-        let choice: u8 = match choice.trim().parse() {
+        let num: i64 = match num.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("\n=================================\nError: Insert a valid option\n=================================\n");
+                println!("\n=================================\n\tError: Enter a valid number\n=================================\n");
                 continue;
             }
         };
 
-        print!("\nInsert a number\n");
-
-        let mut number: String = String::new();
-
-        io::stdin()
-            .read_line(&mut number)
-            .expect("Falha ao ler entrada");
-
-        let number: u64 = match number.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("\n=================================\nError: Insert a valid number\n=================================\n");
-                continue;
-            }
-        };
-
-        match choice {
-            1 => is_it_prime(number),
-            2 => n_prime_number(number),
-            _ => continue,
+        match num % 2 {
+            0 => println!("\n{} is even", num),
+            _ => println!("\n{} is odd", num),
         }
     }
-}
-
-fn is_it_prime(number: u64) {
-    todo!()
-}
-
-fn n_prime_number(number: u64) {
-    todo!()
 }
